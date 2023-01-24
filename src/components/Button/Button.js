c263193027d6b2711889e9570a1edad006fb28cd
ShocKWavEv1 <rodd.sal23@gmail.com>
2023-01-24 04:37:49 -0600

    prevent outline color for being blue

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -19,7 +19,7 @@ export const Button = styled('button')(
       },
       outline: {
         color: 'black',
-        backgroundColor: 'transparent',
+        backgroundColor: 'white',
         border: "solid 2px black",
         padding: "14px 35px",
         boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px;",
