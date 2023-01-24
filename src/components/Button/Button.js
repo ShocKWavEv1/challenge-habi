5c0f84e1ce7b7f810c1b0c35093ebdbeafb44706
ShocKWavEv1 <rodd.sal23@gmail.com>
2023-01-24 04:38:11 -0600

    color white

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -18,7 +18,7 @@ export const Button = styled('button')(
         }
       },
       outline: {
-        color: 'black',
+        color: 'white',
         backgroundColor: 'white',
         border: "solid 2px black",
         padding: "14px 35px",
